4cba2f77ab868f4ea448f7d1ac4dbceef6519f35
junko911 <junkotahara911@gmail.com>
2020-08-10 18:37:33 -0400

    Created edit view for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :find_post, only: [:show, :edit, :update]
+
   def new
     @post = Post.new
   end
 
   def show
-    @post = Post.find(params[:id])
     if params[:like]
       @post.likes += 1
       @post.save
@@ -16,9 +17,21 @@ class PostsController < ApplicationController
     redirect_to post_path(@post)
   end
 
+  def edit
+  end
+  
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+  
   private
-
+  
   def post_params
     params.require(:post).permit(:title, :content, :likes, :blogger_id, :destination_id)
+  end
+
+  def find_post
+    @post = Post.find(params[:id])
   end
 end
