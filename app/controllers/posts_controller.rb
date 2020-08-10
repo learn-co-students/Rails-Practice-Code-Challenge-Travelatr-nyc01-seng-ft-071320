fb0d94d207885a4c2552a1e4f4f500b097f7b317
junko911 <junkotahara911@gmail.com>
2020-08-10 17:58:03 -0400

    Created new view and validation for posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,20 @@
 class PostsController < ApplicationController
+  def new
+    @post = Post.new
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def create
+    @post = Post.create(post_params)
+    redirect_to post_path(@post)
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :content, :likes, :blogger_id, :destination_id)
+  end
 end
